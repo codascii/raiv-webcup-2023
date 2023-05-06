9a0109310d0e60a138b3d6f5aa51aaf09d66e96b
Mohamed Houmadi <dev.codascii@gmail.com>
2023-05-07 01:07:08 +0300

    Code review

--- components/HomeOne/About.js
+++ components/HomeOne/About.js
@@ -2,83 +2,90 @@ import React from "react";
 import Link from "next/link";
 
 const About = () => {
-  return (
-    <>
-      <section className="about-area pb-100">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-6">
-              <div 
-                className="about-img"
-                data-aos="fade-in"
-                data-aos-duration="1200"
-                data-aos-delay="100"
-              >
-                <img src="/images/about-img.png" alt="Image" />
-              </div>
-            </div>
+	return (
+		<>
+			<section className="about-area pb-100">
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col-lg-6">
+							<div
+								className="about-img"
+								data-aos="fade-in"
+								data-aos-duration="1200"
+								data-aos-delay="100"
+							>
+								<img src="/images/about-img.png" alt="Image" />
+							</div>
+						</div>
 
-            <div className="col-lg-6">
-              <div 
-                className="about-content"
-                data-aos="fade-in"
-                data-aos-duration="1200"
-                data-aos-delay="200"
-              >
-                <span>Rêve ou avenir</span>
-                <h2>
-                Chaque rêve est analyser avec une attention particulière aux besoins de l'utilisateur
-                </h2>
-                <p>
-                Chez l'Institut International des Rêves, nous considérons que chaque rêve est unique et que son interprétation doit être personnalisée en fonction des besoins et des attentes de chaque utilisateur
-                </p>
+						<div className="col-lg-6">
+							<div
+								className="about-content"
+								data-aos="fade-in"
+								data-aos-duration="1200"
+								data-aos-delay="200"
+							>
+								<span>Rêve ou avenir</span>
+								<h2>
+									Chaque rêve est analysé avec une attention
+									particulière aux besoins de l'utilisateur
+								</h2>
+								<p>
+									À l'Institut International des Rêves, nous
+									considérons que chaque rêve est unique et
+									que son interprétation doit être
+									personnalisée tout en prévenant les risques
+									psychologique de utilisateur
+								</p>
 
-                <div className="row">
-                  <div className="col-lg-6 col-sm-6">
-                    <ul>
-                      <li>
-                        <i className="flaticon-checked"></i>
-                        Analyse approfondie
-                      </li>
-                      <li>
-                        <i className="flaticon-checked"></i>
-                        Réponses détaillées
-                      </li>
-                      <li>
-                        <i className="flaticon-checked"></i>
-                        Interprétation et explication
-                      </li>
-                    </ul>
-                  </div>
+								<div className="row">
+									<div className="col-lg-6 col-sm-6">
+										<ul>
+											<li>
+												<i className="flaticon-checked"></i>
+												Analyse approfondie
+											</li>
+											<li>
+												<i className="flaticon-checked"></i>
+												Réponses détaillées
+											</li>
+											<li>
+												<i className="flaticon-checked"></i>
+												Interprétation et explication
+											</li>
+										</ul>
+									</div>
 
-                  <div className="col-lg-6 col-sm-6">
-                    <ul>
-                      <li>
-                        <i className="flaticon-checked"></i>
-                        Prédiction réaliste
-                      </li>
-                      <li>
-                        <i className="flaticon-checked"></i>
-                        Pas de stockage des données personnelles
-                      </li>
-                      <li>
-                        <i className="flaticon-checked"></i>
-                        Service gratuit
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+									<div className="col-lg-6 col-sm-6">
+										<ul>
+											<li>
+												<i className="flaticon-checked"></i>
+												Prédiction réaliste
+											</li>
+											<li>
+												<i className="flaticon-checked"></i>
+												Pas de stockage des données
+												personnelles
+											</li>
+											<li>
+												<i className="flaticon-checked"></i>
+												Service gratuit
+											</li>
+										</ul>
+									</div>
+								</div>
 
-                <Link href="/onirix" className="default-btn">
-                  En savoir plus
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+								<Link href="/onirix" className="default-btn">
+									En savoir plus sur Onirix{" "}
+									<i className="bx bx-log-in-circle"></i>
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default About;
--- components/HomeOne/Services.js
+++ components/HomeOne/Services.js
@@ -2,101 +2,106 @@ import React from "react";
 import Link from "next/link";
 
 const servicesData = [
-  {
-    iconName: "flaticon-chip",
-    title: "Dans la psychologie",
-    shortText:
-      "Notre intélligence artificielle Pour aider à interpréter les rêves de manière thérapeutique.",
-    viewDetails: "/service-details",
-    aosDelay: "100",
-  },
-  {
-    iconName: "flaticon-vr",
-    title: "La créativité",
-    shortText:
-      "Onirix pourrait être utilisé comme source d'inspiration pour les écrivains, les artistes et les cinéastes.",
-    viewDetails: "/service-details",
-    aosDelay: "200",
-  },
-  {
-    iconName: "flaticon-blockchain",
-    title: "La sécurité",
-    shortText:
-      "L'intelligence articifielle peut être utilisé afin de prévenir des accidents ou des catastrophes naturelles.",
-    viewDetails: "/service-details",
-    aosDelay: "300",
-  },
-  {
-    iconName: "flaticon-target",
-    title: "En marketing",
-    shortText:
-      "Permettre une meilleure compréhension des préférences et des désirs des consommateurs.",
-    viewDetails: "/service-details",
-    aosDelay: "400",
-  },
-  {
-    iconName: "flaticon-choice",
-    title: "Les statistique",
-    shortText:
-      "Notre intelligence artificielle permet d'alimenter des bases de données afin de réaliser des statistiques sur les rêves.",
-    viewDetails: "/service-details",
-    aosDelay: "500",
-  },
-  {
-    iconName: "flaticon-deep-learning",
-    title: "La recherche",
-    shortText:
-      "la recherche scientifique pour étudier les processus mentaux impliqués dans la production des rêves.",
-    viewDetails: "/service-details",
-    aosDelay: "600",
-  },
+	{
+		iconName: "flaticon-chip",
+		title: "Dans la psychologie",
+		shortText:
+			"Notre intélligence artificielle pourrait aider à interpréter les rêves pour des besions thérapeutique.",
+		viewDetails: "/service-details",
+		aosDelay: "100",
+	},
+	{
+		iconName: "flaticon-vr",
+		title: "La créativité",
+		shortText:
+			"Onirix pourrait être utilisé comme source d'inspiration pour les écrivains, les artistes et les cinéastes.",
+		viewDetails: "/service-details",
+		aosDelay: "200",
+	},
+	{
+		iconName: "flaticon-blockchain",
+		title: "La sécurité",
+		shortText:
+			"L'intelligence articifielle peut être utilisé afin de prévenir des accidents ou des catastrophes naturelles.",
+		viewDetails: "/service-details",
+		aosDelay: "300",
+	},
+	{
+		iconName: "flaticon-target",
+		title: "En marketing",
+		shortText:
+			"Permettre une meilleure compréhension des préférences et des désirs des consommateurs.",
+		viewDetails: "/service-details",
+		aosDelay: "400",
+	},
+	{
+		iconName: "flaticon-choice",
+		title: "Les statistique",
+		shortText:
+			"Notre IA permet d'alimenter des bases de données pour réaliser des statistiques sur les rêves.",
+		viewDetails: "/service-details",
+		aosDelay: "500",
+	},
+	{
+		iconName: "flaticon-deep-learning",
+		title: "La recherche",
+		shortText:
+			"la recherche scientifique pour étudier les processus mentaux impliqués dans la production des rêves.",
+		viewDetails: "/service-details",
+		aosDelay: "600",
+	},
 ];
 
 const Services = () => {
-  return (
-    <section className="offer-area pt-100 pb-70">
-      <div className="container">
-        <div className="section-title">
-          <span>Domaine d'application</span>
-          <h2>D'autres domaines d'application pour Onirix</h2>
-          <p>
-          En plus de son fonctionnement d'origine pourraient être envisagés afin d'exploiter au mieux les capacités de cette IA, et ainsi répondre à un large éventail de besoins.
-          </p>
-        </div>
+	return (
+		<section className="offer-area pt-100 pb-70">
+			<div className="container">
+				<div className="section-title">
+					<span>Domaine d'application</span>
+					<h2>D'autres domaines d'application pour Onirix</h2>
+					<p>
+						En plus de son fonctionnement d'origine, d'autres
+						domaines d'application pourraient être envisagés afin
+						d'exploiter au mieux les capacités de cette IA, et ainsi
+						répondre à un large éventail de besoins.
+					</p>
+				</div>
 
-        <div className="row justify-content-center">
-          {servicesData &&
-            servicesData.slice(0, 6).map((value, i) => (
-              <div
-                className="col-lg-4 col-sm-6"
-                key={i}
-                data-aos="fade-in"
-                data-aos-duration="1200"
-                data-aos-delay={value.aosDelay}
-              >
-                <div className="single-offer">
-                  <i className={value.iconName}></i>
-                  <h3>
-                    <Link href={value.viewDetails}>{value.title}</Link>
-                  </h3>
-                  <p>{value.shortText}</p>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
+				<div className="row justify-content-center">
+					{servicesData &&
+						servicesData.slice(0, 6).map((value, i) => (
+							<div
+								className="col-lg-4 col-sm-6"
+								key={i}
+								data-aos="fade-in"
+								data-aos-duration="1200"
+								data-aos-delay={value.aosDelay}
+							>
+								<div className="single-offer">
+									<i className={value.iconName}></i>
+									<h3>
+										<Link href={value.viewDetails}>
+											{value.title}
+										</Link>
+									</h3>
+									<p>{value.shortText}</p>
+								</div>
+							</div>
+						))}
+				</div>
+			</div>
 
-      {/* Shape Images */}
-      <div className="offer-shape">
-        <img src="/images/shape/services-shape/1.png" alt="Image" />
-        <img src="/images/shape/services-shape/2.png" alt="Image" />
-        <img src="/images/shape/services-shape/3.png" alt="Image" />
-        <img src="/images/shape/services-shape/4.png" alt="Image" />
-        <img src="/images/shape/services-shape/5.png" alt="Image" />
-        <img src="/images/shape/services-shape/6.png" alt="Image" />
-      </div>
-    </section>
-  );
+			{/* Shape Images */}
+			<div className="offer-shape">
+				<img src="/images/shape/services-shape/1.png" alt="Image" />
+				<img src="/images/shape/services-shape/2.png" alt="Image" />
+				<img src="/images/shape/services-shape/3.png" alt="Image" />
+				<img src="/images/shape/services-shape/4.png" alt="Image" />
+				<img src="/images/shape/services-shape/5.png" alt="Image" />
+				<img src="/images/shape/services-shape/6.png" alt="Image" />
+			</div>
+		</section>
+	);
 };
 
 export default Services;
