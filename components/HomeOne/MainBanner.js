87dcf55e410fbaa9852c8aef6255914d1b8f0306
Mohamed Houmadi <dev.codascii@gmail.com>
2023-05-06 16:26:07 +0300

    Modification de la page d'accueil

--- components/HomeOne/MainBanner.js
+++ components/HomeOne/MainBanner.js
@@ -2,93 +2,119 @@ import React from "react";
 import Link from "next/link";
 
 const MainBanner = () => {
-  return (
-    <>
-      <section className="main-banner-area main-banner-area-one">
-        <div className="container-fluid">
-          <div className="row align-items-center">
-            <div className="col-lg-6">
-              <div className="banner-text">
-                <h1
-                  data-aos="fade-in"
-                  data-aos-duration="1200"
-                  data-aos-delay="100"
-                >
-                  Institut Internationnal des rêves
-                </h1>
+	return (
+		<>
+			<section className="main-banner-area main-banner-area-one">
+				<div className="container-fluid">
+					<div className="row align-items-center">
+						<div className="col-lg-6">
+							<div className="banner-text">
+								<h1
+									data-aos="fade-in"
+									data-aos-duration="1200"
+									data-aos-delay="100"
+								>
+									Institut Internationnal des rêves
+								</h1>
 
-                <p
-                  data-aos="fade-in"
-                  data-aos-duration="1200"
-                  data-aos-delay="200"
-                >
-                  Et si vos rêves ne se limitaient pas à être de simples illusions de l'esprit ? Et s'ils étaient capables
-de prédire l'avenir ? C'est exactement ce que nos chercheurs grâce à la création de l'IA ONIRIX. 
-                </p>
+								<p
+									data-aos="fade-in"
+									data-aos-duration="1200"
+									data-aos-delay="200"
+								>
+									Et si vos rêves ne se limitaient pas à être
+									de simples illusions de l'esprit ? Et s'ils
+									étaient capables de prédire l'avenir ? C'est
+									exactement ce que nos chercheurs ont
+									découverts. Grâce à des années de recherche
+									et de développement, les scientifiques ont
+									développé Onirix, une inteligence
+									artificielle avancée capable d'analyser et
+									de décoder les rêves des utilisateurs pour
+									prédire leur avenir.
+								</p>
 
-                <div 
-                  className="banner-btn"
-                  data-aos="fade-in"
-                  data-aos-duration="1200"
-                  data-aos-delay="300"
-                >
-                  <Link href="/dreams-prediction" className="default-btn">
-                    Essayez ONIRIX
-                  </Link>
+								<div
+									className="banner-btn"
+									data-aos="fade-in"
+									data-aos-duration="1200"
+									data-aos-delay="300"
+								>
+									<Link
+										href="/dreams-prediction"
+										className="default-btn"
+									>
+										Essayez Onirix
+									</Link>
 
-                  <Link href="/contact" className="default-btn">
-                    Contact Us
-                  </Link>
-                </div>
-              </div>
-            </div>
+									<Link
+										href="/contact"
+										className="default-btn"
+									>
+										Contactez-nous
+									</Link>
+								</div>
+							</div>
+						</div>
 
-            <div className="col-lg-6">
-              {/* Main Image */}
-              <div className="banner-main-img banner-one-main-img">
-                <img src="/images/home-one/main-img1.png" alt="Image" />
-              </div>
+						<div className="col-lg-6">
+							{/* Main Image */}
+							<div className="banner-main-img banner-one-main-img">
+								<img
+									src="/images/home-one/main-img1.png"
+									alt="Image"
+								/>
+							</div>
 
-              {/* Banner Shape Images */}
-              <div className="banner-img">
-                <img
-                  className="animate__animated animate__fadeInUp animate__fast"
-                  src="/images/home-one/shape1.png"
-                  alt="Image"
-                />
-                <img
-                  className="animate__animated animate__fadeInUp animate__fast"
-                  src="/images/home-one/shape2.png"
-                  alt="Image"
-                />
-                <img
-                  className="animate__animated animate__fadeInRight animate__fast"
-                  src="/images/home-one/shape3.png"
-                  alt="Image"
-                />
-                <img
-                  className="animate__animated animate__fadeInRight animate__fast"
-                  src="/images/home-one/shape4.png"
-                  alt="Image"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+							{/* Banner Shape Images */}
+							<div className="banner-img">
+								<img
+									className="animate__animated animate__fadeInUp animate__fast"
+									src="/images/home-one/shape1.png"
+									alt="Image"
+								/>
+								<img
+									className="animate__animated animate__fadeInUp animate__fast"
+									src="/images/home-one/shape2.png"
+									alt="Image"
+								/>
+								<img
+									className="animate__animated animate__fadeInRight animate__fast"
+									src="/images/home-one/shape3.png"
+									alt="Image"
+								/>
+								<img
+									className="animate__animated animate__fadeInRight animate__fast"
+									src="/images/home-one/shape4.png"
+									alt="Image"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        {/* Shape Images */}
-        <div className="over-shape">
-          <img src="/images/home-one/shape/animate1.png" alt="Image" />
-          <img src="/images/home-one/shape/animate2.png" alt="Image" />
-          <img src="/images/home-one/shape/animate3.png" alt="Image" />
-        </div>
+				{/* Shape Images */}
+				<div className="over-shape">
+					<img
+						src="/images/home-one/shape/animate1.png"
+						alt="Image"
+					/>
+					<img
+						src="/images/home-one/shape/animate2.png"
+						alt="Image"
+					/>
+					<img
+						src="/images/home-one/shape/animate3.png"
+						alt="Image"
+					/>
+				</div>
 
-        <div className="white-shape">
-          <img src="/images/home-one/bottom-shape.png" alt="Image" />
-        </div>
-      </section>
-    </>
-  );
+				<div className="white-shape">
+					<img src="/images/home-one/bottom-shape.png" alt="Image" />
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default MainBanner;
