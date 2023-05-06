5df43b1c4e412d166460c40e785c3174e48b1c25
SAIDALI Raphael <raphael.saidali@gmail.com>
2023-05-06 20:44:45 +0300

    Amélioration du contenu

--- components/Common/CTA.js
+++ components/Common/CTA.js
@@ -7,12 +7,12 @@ const CTA = () => {
 			<div className="cta-area ptb-100">
         <div className="container">
           <div className="cta-content">
-            <span>So What is Next?</span>
-            <h3>Are You Ready? Let's get to work!</h3>
+            <span>QUELLE EST LA PROCHAINE ÉTAPE ?</span>
+            <h3>Êtes-vous prêt à tester Onirix ?</h3>
           </div>
           <div className="cta-btn-box">
             <Link href="/contact" className="default-btn">
-              Contact Us
+              Essayer Onirix
             </Link>
           </div>
         </div>
