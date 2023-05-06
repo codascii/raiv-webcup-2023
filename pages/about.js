df2470bed8ab735518e8b36ebca8ca189e5cbf2d
Mohamed Houmadi <dev.codascii@gmail.com>
2023-05-06 19:43:51 +0300

    Code review

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,8 @@ import MakeYourBusiness from "../components/Common/MakeYourBusiness";
 import Testimonials from "../components/Common/Testimonials";
 import PartnerSlider from "../components/Common/PartnerSlider";
 import Footer from "../components/Layouts/Footer";
+import TeamTwo from "../components/Common/TeamTwo";
+import Partner from "../components/Common/Partner";
 
 export default function About2() {
 	return (
@@ -21,10 +23,13 @@ export default function About2() {
 			/>
 
 			<About />
+			<TeamTwo />
+			<div className="pb-50">
+				<Partner />
+			</div>
+			{/* <WhyChooseUs /> */}
 
-			<WhyChooseUs />
-
-			<MakeYourBusiness />
+			{/* <MakeYourBusiness /> */}
 
 			<Testimonials />
 
