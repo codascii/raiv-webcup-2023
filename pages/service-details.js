ca7aed770e9c43549d4993e70b056f007f81e649
Mohamed Houmadi <dev.codascii@gmail.com>
2023-05-07 03:15:56 +0300

    Ajout des données de la page "Service Detail"

--- pages/service-details.js
+++ pages/service-details.js
@@ -7,10 +7,11 @@ import Footer from "../components/Layouts/Footer";
 const servicesDetailData = [
 	{
 		key: "psychologie",
-		image: "",
+		image: "psychologie",
 		secondaryImage: "",
 		title: "La psychologie",
-		subtitle: "",
+		subtitle:
+			"Comprendre le pouvoir de la psychologie pour interpréter les rêves avec Onirix",
 		content: [
 			"La psychologie est une branche de la science qui étudie le comportement humain et les processus mentaux. Elle a pour objectif de comprendre comment les gens pensent, ressentent, agissent et interagissent avec leur environnement. La psychologie peut aider les individus à mieux comprendre leur propre comportement et à trouver des moyens de s'adapter aux défis de la vie.",
 			"Onirix utilise des techniques de psychologie pour aider les utilisateurs à interpréter les significations de leurs rêves. Les rêves sont souvent considérés comme un reflet de l'inconscient et peuvent offrir des aperçus précieux sur les désirs, les peurs et les préoccupations qui peuvent être difficiles à comprendre dans l'état de veille.",
@@ -22,25 +23,37 @@ const servicesDetailData = [
 	},
 	{
 		key: "ui-ux-design",
-		image: "",
+		image: "UI-UX",
 		secondaryImage: "",
 		title: "UI/UX Design",
-		subtitle: "",
-		content: [],
+		subtitle:
+			"Onirix : une expérience utilisateur fluide et intuitive pour l'analyse des rêves grâce à un design UI/UX bien pensé",
+		content: [
+			"Onirix est une plateforme d'analyse de rêves qui offre une expérience utilisateur fluide et intuitive grâce à un design UI/UX bien pensé. L'équipe de conception de Onirix a pris en compte tous les détails pour rendre l'utilisation de la plateforme facile et agréable pour les utilisateurs.",
+			"L'interface utilisateur de Onirix est claire et bien organisée, avec une navigation facile à comprendre pour les utilisateurs. Le design est minimaliste et épuré, avec des couleurs apaisantes pour encourager la détente et la réflexion. Les boutons et les icônes sont également clairement étiquetés pour éviter toute confusion ou malentendu.",
+			"En ce qui concerne l'expérience utilisateur, Onirix est conçu pour être accessible et facile à utiliser, même pour les débutants. Les processus d'inscription et de création de profil sont simples et rapides, avec des instructions claires pour guider les utilisateurs tout au long du processus. Les utilisateurs peuvent facilement ajouter des descriptions de rêves, visualiser des analyses et recevoir des prédictions grâce à l'interface conviviale de Onirix.",
+			"En somme, le design UI/UX de Onirix est bien pensé pour offrir une expérience utilisateur agréable et facile à utiliser, tout en offrant des fonctionnalités de pointe pour l'analyse des rêves. L'objectif est de rendre l'analyse des rêves accessible à tous, et le design de Onirix y contribue grandement.",
+		],
 		secondaryContent: ["Ergonomie", "Expérience utilisateur"],
 	},
 	{
 		key: "creativity",
-		image: "",
+		image: "creativiter",
 		secondaryImage: "",
 		title: "La créativité",
-		subtitle: "",
-		content: [],
+		subtitle: "Libérez votre créativité avec l'IA avancée d'ONIRIX",
+		content: [
+			"Onirix est une plateforme qui encourage la créativité dans l'interprétation et l'analyse des rêves. En effet, l'objectif de Onirix est de permettre aux utilisateurs d'explorer leur monde intérieur et de découvrir des aspects d'eux-mêmes qu'ils ne connaissaient peut-être pas.",
+			"Pour stimuler la créativité, Onirix propose des outils d'analyse de rêves avancés, qui permettent de détecter des modèles et des tendances dans les rêves des utilisateurs. Ces outils sont conçus pour offrir des analyses approfondies qui peuvent aider les utilisateurs à interpréter leurs rêves de manière plus créative.",
+			"Onirix encourage également la créativité en permettant aux utilisateurs de partager leurs rêves avec d'autres personnes et de recevoir des commentaires constructifs de la part de la communauté. Cela peut être utile pour encourager l'imagination et la réflexion créative.",
+			"En outre, Onirix propose des fonctionnalités uniques qui permettent aux utilisateurs d'explorer leur créativité de manière plus approfondie. Par exemple, les utilisateurs peuvent utiliser des images, des sons et d'autres éléments multimédias pour enrichir leur expérience de rêve et stimuler leur créativité.",
+			"En somme, Onirix est une plateforme qui encourage la créativité dans l'analyse et l'interprétation des rêves. Grâce à des outils avancés et des fonctionnalités uniques, Onirix permet aux utilisateurs d'explorer leur monde intérieur de manière plus créative et stimulante.",
+		],
 		secondaryContent: ["Innovation", "Intelligence artificielle"],
 	},
 	{
 		key: "marketing",
-		image: "",
+		image: "marketing",
 		secondaryImage: "",
 		title: "Marketing",
 		subtitle:
@@ -55,16 +68,24 @@ const servicesDetailData = [
 	},
 	{
 		key: "ingeniering",
-		image: "",
+		image: "production",
 		secondaryImage: "",
 		title: "L'ingénierie de production",
-		subtitle: "",
-		content: [],
+		subtitle:
+			"De l'élaboration à la production: comment Onirix déploie son IA avancée pour aider les utilisateurs à comprendre leurs rêves",
+		content: [
+			"La mise en production d'ONIRIX est une étape cruciale dans le processus de développement de cette intelligence artificielle. Après des mois de développement, de tests et d'itérations, il est enfin temps de mettre ONIRIX à la disposition du public.",
+			"La mise en production implique plusieurs étapes importantes, telles que la configuration de l'infrastructure nécessaire pour exécuter ONIRIX, la préparation de la base de données pour stocker les données de rêve, la mise en place de la sécurité pour protéger les informations des utilisateurs et la préparation de l'interface utilisateur pour une utilisation facile et intuitive.",
+			"Une fois que toutes ces étapes sont terminées, ONIRIX est prêt à être lancé. Cependant, cela ne signifie pas que le travail est terminé. La mise en production est un processus continu qui nécessite une surveillance constante et des mises à jour régulières pour assurer le bon fonctionnement de l'IA.",
+			"Le processus de mise en production peut être complexe et nécessite une grande expertise en matière de développement et d'infrastructure informatique. C'est pourquoi ONIRIX travaille en étroite collaboration avec des experts du domaine pour s'assurer que chaque étape est effectuée de manière optimale.",
+			"Le succès de la mise en production dépend également de l'adoption et de la satisfaction des utilisateurs. ONIRIX s'efforce donc de fournir une expérience utilisateur exceptionnelle en offrant un support client de qualité, en écoutant les commentaires des utilisateurs et en apportant des améliorations continues à l'IA.",
+			"En somme, la mise en production d'ONIRIX est une étape cruciale pour offrir aux utilisateurs une expérience de rêve unique. Elle exige une planification minutieuse, une grande expertise technique et un engagement continu envers l'amélioration et la satisfaction des utilisateurs.",
+		],
 		secondaryContent: ["Psychologie", "Langue"],
 	},
 	{
 		key: "research",
-		image: "",
+		image: "searching",
 		secondaryImage: "",
 		title: "La recherche",
 		subtitle:
